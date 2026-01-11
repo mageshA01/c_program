2568b59aec53890f92f06c3b8c650c9728e736dc
mageshA01 <magesharun2094@gmail.com>
2026-01-11 19:18:04 +0000

    mul fn added

--- Question-1/main.c
+++ Question-1/main.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-int main(){
-  int a=100,b=200;
-printf("%d",add(a,b));
-printf("%d",mul(a,b));
-return 0;
-}
+
 int add(int x,int y){
   return x+y;
 }
 int mul(int x,int y){
   return x*y;
+}
+int main(){
+  int a=100,b=200;
+printf("%d",add(a,b));
+printf("%d",mul(a,b));
+return 0;
 }
