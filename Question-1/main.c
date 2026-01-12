51016eba0500f8262fc557d0204edc39c8e27f2d
mageshA01 <magesharun2094@gmail.com>
2026-01-12 09:16:25 +0530

    age verified fn

--- Question-1/main.c
+++ Question-1/main.c
@@ -12,3 +12,12 @@ printf("%d",add(a,b));
 printf("%d",mul(a,b));
 return 0;
 }
+//fn for age verfication
+int verify_age(int age){
+  if(age>=18){
+    return 1; //age valid
+  }
+  else{
+    return 0; //age not valid
+  }
+}
